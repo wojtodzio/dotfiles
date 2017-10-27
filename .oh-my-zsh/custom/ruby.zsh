ddfdaf13c3e206431ad1f88c69e80cc15eccf176
Wojtek Wrona <wojtodzio@gmail.com>
2017-10-27 11:39:27 +0200

    Fix inline_global_ruby

--- .oh-my-zsh/custom/ruby.zsh
+++ .oh-my-zsh/custom/ruby.zsh
@@ -4,7 +4,7 @@ alias re='pry-remote'
 
 # Eval passed commands (inside HEREDOC) with global version of Ruby
 inline_global_ruby() {
-  export ASDF_RUBY_VERSION="$(cat ~/.tool-versions G ruby | cut -d ' ' -f 2)"
+  export ASDF_RUBY_VERSION="$(cat ~/.tool-versions | grep ruby | cut -d ' ' -f 2)"
 
   while IFS="\n" read -r read_heredoc_line; do
     eval "$read_heredoc_line"
