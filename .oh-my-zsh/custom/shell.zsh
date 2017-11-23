5017b24a26413c532cc86fe3fff252031efc7c4d
Wojtek Wrona <wojtodzio@gmail.com>
2017-11-23 12:13:25 +0100

    Add temporary workaround for _rails_command in expand-alias

--- .oh-my-zsh/custom/shell.zsh
+++ .oh-my-zsh/custom/shell.zsh
@@ -18,7 +18,7 @@ ea() {
   unset 'functions[_expand-aliases]'
   functions[_expand-aliases]="$@"
   (($+functions[_expand-aliases]))
-  echo "${functions[_expand-aliases]#$'\t'}"
+  echo "${functions[_expand-aliases]#$'\t'}" | sed 's/_rails_command/rails/'
 }
 
 # Use ripgrep as a pipe-grep
